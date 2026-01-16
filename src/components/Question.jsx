5400f6e21bd25316f825c1bc6c3ece7d06ac37c4
Bolazcoding <bolajiadelabu0@gmail.com>
2026-01-17 00:18:11 +0100

    initial commit

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -2,6 +2,9 @@ function Question({ question }) {
   return (
     <div>
       <h1 className="text-text-primary font-semibold">{question.question}</h1>
+      <p className="text-error mt-5 font-semibold">
+        Building in progress... , check back later
+      </p>
     </div>
   );
 }
